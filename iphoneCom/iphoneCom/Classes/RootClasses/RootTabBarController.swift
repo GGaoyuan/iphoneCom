2190603b712c2c5bf5a8e47b84a1f7309ea686d5
gaoyuan <707216098@qq.com>
2018-03-07 19:28:31 +0800

    123

--- iphoneCom/iphoneCom/Classes/RootClasses/RootTabBarController.swift
+++ iphoneCom/iphoneCom/Classes/RootClasses/RootTabBarController.swift
@@ -13,6 +13,30 @@ class RootTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let vc1 = ViewControllerF()
+        vc1.tabBarItem.title = "1"
+        let vc2 = ViewControllerS()
+        vc2.tabBarItem.title = "2"
+        self.addChildViewController(vc1)
+        self.addChildViewController(vc2)
         
     }
 }
+
+
+//YXNavigationViewController *ctrlNav = [[YXNavigationViewController alloc]
+//    initWithRootViewController:ctrl];
+//ctrlNav.tabBarItem.image = [UIImage imageNamed:image];
+//ctrlNav.tabBarItem.title = title;
+//ctrlNav.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+//[self addChildViewController:ctrlNav];
+//[_dock tabBarItem:ctrlNav.tabBarItem index:index];
+
+//        YXHomeViewController *homelVC = [[YXHomeViewController alloc] init];
+//        [self addEveryOneController:homelVC image:@"tabbar_home_background"
+//        selectedImage:@"tabbar_home_hl_background"  title:@"天眼" index:0];
+//
+//        //论坛
+//        YXBBSViewController *bbsVC = [[YXBBSViewController alloc] init];
+//        [self addEveryOneController:bbsVC image:@"tabbar_bbs_background"
+//        selectedImage:@"tabbar_bbs_hl_background"  title:@"社区" index:1];
