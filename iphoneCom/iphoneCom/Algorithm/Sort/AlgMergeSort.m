6d6009459b6d7adc85a31a42f3ab31176a0ae1b2
gaoyuan <gaoyuan@p2peye.com>
2018-06-25 19:28:18 +0800

    归并排序不会写啊

--- iphoneCom/iphoneCom/Algorithm/Sort/AlgMergeSort.m
+++ iphoneCom/iphoneCom/Algorithm/Sort/AlgMergeSort.m
@@ -13,52 +13,11 @@
 
 + (void)sort:(NSMutableArray *)array {
     [AlgorithmTool show:array];
-    NSMutableArray *loArray = [NSMutableArray arrayWithCapacity:2];
-    NSMutableArray *hiArray = [NSMutableArray arrayWithCapacity:2];
-    for (NSInteger i = 0; i < array.count; i++) {
-        if (i % 4 == 0) {
-            if (loArray.count == 2 && hiArray.count == 2) {
-                if (loArray[0] > loArray[1]) {
-                    [loArray exchangeObjectAtIndex:0 withObjectAtIndex:1];
-                }
-                if (hiArray[0] > hiArray[1]) {
-                    [hiArray exchangeObjectAtIndex:0 withObjectAtIndex:1];
-                }
-                NSLog(@"-------");
-                [AlgorithmTool show:loArray];
-                [AlgorithmTool show:hiArray];
-                NSLog(@"-------");
-            }
-            
-            [loArray removeAllObjects];
-            [hiArray removeAllObjects];
-            
-            [loArray addObject:array[i]];
-        }
-        else if (i % 4 == 1) {
-            [loArray addObject:array[i]];
-        }
-        else if (i % 4 == 2) {
-            [hiArray addObject:array[i]];
-        }
-        else if (i % 4 == 3) {
-            [hiArray addObject:array[i]];
-        }
-        
-        
-        
-    }
-    
     
 }
 
-+ (void)merge:(NSMutableArray *)a low:(NSInteger)lo middle:(NSInteger)mid high:(NSInteger)hi {
-//    NSInteger i = lo;
-//    NSInteger j = mid + 1;
-//    
-//    for (NSInteger k = lo; k <= hi; k++) {
-//        
-//    }
++ (void)merge:(NSMutableArray *)array low:(NSInteger)lo middle:(NSInteger)mid high:(NSInteger)hi {
+
 }
 
 @end
