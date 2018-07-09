02541028bd6c670328d29f29b1778856b2293cac
gaoyuan <gaoyuan@moer.cn>
2018-07-10 01:57:23 +0800

    as

--- iphoneCom/iphoneCom/GCD/GCDEntrance.m
+++ iphoneCom/iphoneCom/GCD/GCDEntrance.m
@@ -19,33 +19,76 @@
     
     NSLog(@"%@",[NSThread currentThread]);
     
-//    dispatch_queue_t sq = dispatch_queue_create("1", DISPATCH_QUEUE_SERIAL);
-//    dispatch_sync(sq, ^{
-//        for (int i = 0; i < 5; i++) {
-//            NSLog(@"1----%d",i);
-//            NSLog(@"%@",[NSThread currentThread]);
-//        }
-//    });
-//    dispatch_sync(sq, ^{
-//        for (int i = 0; i < 5; i++) {
-//            NSLog(@"2----%d",i);
-//            NSLog(@"%@",[NSThread currentThread]);
-//        }
-//    });
-//    dispatch_sync(sq, ^{
-//        for (int i = 0; i < 5; i++) {
-//            NSLog(@"3----%d",i);
-//            NSLog(@"%@",[NSThread currentThread]);
-//        }
-//    });
-//    dispatch_sync(sq, ^{
-//        for (int i = 0; i < 5; i++) {
-//            NSLog(@"4----%d",i);
-//            NSLog(@"%@",[NSThread currentThread]);
-//        }
-//    });
-    
-    
+    [self sync_c];
+}
+
+- (void)async_s {
+    dispatch_queue_t sq = dispatch_queue_create("1", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"2----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_async(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"3----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_async(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"4----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+}
+
+- (void)async_c {
+    dispatch_queue_t cq = dispatch_queue_create("1", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(cq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"2----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_async(cq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"3----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_async(cq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"4----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+}
+
+- (void)sync_s {
+    dispatch_queue_t sq = dispatch_queue_create("1", DISPATCH_QUEUE_SERIAL);
+    dispatch_sync(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"2----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_sync(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"3----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+    dispatch_sync(sq, ^{
+        for (int i = 0; i < 5; i++) {
+            NSLog(@"4----%d",i);
+            NSLog(@"%@",[NSThread currentThread]);
+        }
+    });
+}
+
+- (void)sync_c {
     dispatch_queue_t cq = dispatch_queue_create("1", DISPATCH_QUEUE_CONCURRENT);
     dispatch_sync(cq, ^{
         for (int i = 0; i < 5; i++) {
@@ -66,4 +109,5 @@
         }
     });
 }
+
 @end
