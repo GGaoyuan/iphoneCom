87d5a35d045acce6faeac4033851784ea01ca7e4
gaoyuan <gaoyuan@p2peye.com>
2018-08-09 11:55:10 +0800

    1111

--- iphoneCom/iphoneCom/Home/YDHomeViewController.m
+++ iphoneCom/iphoneCom/Home/YDHomeViewController.m
@@ -7,14 +7,11 @@
 //
 
 #import "YDHomeViewController.h"
-#import "YDDatabaseTool.h"
-#import "TestObject.h"
-#import "MobClick.h"
-#import "MobClickHelper.h"
+#import "YDHomeViewController+Test.h"
 
 @interface YDHomeViewController ()
 
-@property (nonatomic, strong) NSObject *test;
+@property (nonatomic, strong) NSObject *obj;
 
 @end
 
@@ -22,46 +19,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"home";
-    self.view.backgroundColor = [UIColor redColor];
-    
-    
-    
-    UIButton *button = ({
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button setTitle:@"UmengTest" forState:UIControlStateNormal];
-        button.backgroundColor = [UIColor yellowColor];
-        button.titleLabel.font = [UIFont systemFontOfSize:12];
-        [button addTarget:self action:@selector(testAction) forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview:button];
-        button;
-    });
-    button.frame = CGRectMake(0, 70, 100, 100);
-    
-    TestObject *test = [TestObject new];
-    test.integer = 1;
-    test.string = @"string";
-    test.number = @(3);
-    
-//    [MobClickHelper load];
-    
-    NSDictionary *dic = [YDDatabaseTool formatQueryModel:test blackList:@[@"integer"] maper:@{@"string":@"test"}];
-//    NSLog(@"%@",dic);
-    
+    [self yoyo];
 }
 
 
-- (void)testAction {
-
-    [MobClick event:@"gaoyuan"];
-}
-
-- (void)request {
-    self.test = [NSObject new];
-//    printf("retain count = %ld\n",CFGetRetainCount((__bridge CFTypeRef)(self.test)));
-    
-    NSMutableArray *obj = [NSMutableArray array];
-    printf("retain count = %ld\n",CFGetRetainCount((__bridge CFTypeRef)(obj)));
-}
 
 @end
