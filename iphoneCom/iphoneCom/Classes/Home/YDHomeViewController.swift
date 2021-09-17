027e9566dfedd4c6c8b600d3867123221fb36445
gaoyuan <707216098@qq.com>
2021-09-17 18:02:01 +0800

    add

--- iphoneCom/iphoneCom/Classes/Home/YDHomeViewController.swift
+++ iphoneCom/iphoneCom/Classes/Home/YDHomeViewController.swift
@@ -21,8 +21,19 @@ class YDHomeViewController: YDViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20190811%2F7dc7dc291bea4e94b6b8444cd8687820.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1634464492&t=5ee3401249e6eae619948b4bc6e62e38"
         
-        
+        let imageView = { () -> UIImageView in
+            let imageView = UIImageView()
+            imageView.backgroundColor = .clear
+            view.addSubview(imageView)
+            return imageView
+        }()
+        imageView.snp.makeConstraints { m in
+            m.size.equalTo(CGSize(width: 100, height: 100))
+            m.left.top.equalTo(100)
+        }
+        imageView.kf.setImage(with: URL(string: url))
         
         dpEntrance()
     }
