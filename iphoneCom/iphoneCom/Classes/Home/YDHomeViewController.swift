4cdbd3a5211f81e619697d309aac671adf7d770a
gaoyuan <707216098@qq.com>
2021-09-17 17:17:21 +0800

    add

--- iphoneCom/iphoneCom/Classes/Home/YDHomeViewController.swift
+++ iphoneCom/iphoneCom/Classes/Home/YDHomeViewController.swift
@@ -17,140 +17,17 @@ let Height: CGFloat = 36
 let Space: CGFloat = 20
 let RightMargin: CGFloat = 50
 class YDHomeViewController: YDViewController {
-
-    var attentionBtn: KTVPersonpageAttentionView!
-    var chatBtn: KTVPersonpageChatView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        WWDCShare.main()
-        
-        let btn = { () -> UIButton in
-            let button = UIButton(type: .custom)
-            button.backgroundColor = .red
-//            button.addTarget(self, action: #selector(testAction), for: .touchUpInside)
-            button.adjustsImageWhenHighlighted = false
-            view.addSubview(button)
-            return button
-        }()
-        btn.snp.makeConstraints { (m) in
-            m.size.equalTo(CGSize(width:100, height: 100))
-            m.centerY.centerX.equalToSuperview()
-        }
         
         
-        
-        let container = { () -> UIView in
-            let kView = UIView()
-            kView.backgroundColor = .white
-            view.addSubview(kView)
-            return kView
-        }()
-        container.snp.makeConstraints { m in
-            m.top.right.equalToSuperview()
-            m.left.equalTo(50)
-            m.height.equalTo(200)
-        }
-        
-        
-        
-        
-        
-        
-        chatBtn = KTVPersonpageChatView()
-        container.addSubview(chatBtn)
-        chatBtn.snp.makeConstraints { m in
-            m.width.height.equalTo(Height)
-            m.centerY.equalToSuperview()
-            m.right.equalTo(-RightMargin)
-        }
-        attentionBtn = KTVPersonpageAttentionView()
-        container.addSubview(attentionBtn)
-        attentionBtn.snp.makeConstraints { m in
-            m.left.equalToSuperview()
-            m.right.equalTo(chatBtn.snp.left).offset(-Space)
-            m.centerY.equalToSuperview()
-            m.height.equalTo(Height)
-        }
-        
-        
-        attentionBtn.expanded(true)
-        chatBtn.expanded(false)
-        
-        
-        let _ = { () -> UIButton in
-            let button = UIButton(type: .custom)
-            button.backgroundColor = .clear
-            button.addTarget(self, action: #selector(chatBtnAction), for: .touchUpInside)
-            chatBtn.addSubview(button)
-            button.snp.makeConstraints { m in
-                m.edges.equalToSuperview()
-            }
-            return button
-        }()
-        let _ = { () -> UIButton in
-            let button = UIButton(type: .custom)
-            button.backgroundColor = .clear
-            button.addTarget(self, action: #selector(attentionBtnAction), for: .touchUpInside)
-            attentionBtn.addSubview(button)
-            button.snp.makeConstraints { m in
-                m.edges.equalToSuperview()
-            }
-            return button
-        }()
         
         dpEntrance()
     }
     
     
-}
-
-
-let k: TimeInterval = 10
-extension YDHomeViewController {
-    @objc func attentionBtnAction() {
-        //外形改变
-        UIView.animate(withDuration: 55 / k) {
-            self.attentionBtn.snp.remakeConstraints { m in
-                m.left.centerY.equalToSuperview()
-                m.width.height.equalTo(Height)
-            }
-            self.chatBtn.snp.remakeConstraints { m in
-                m.height.equalTo(Height)
-                m.centerY.equalToSuperview()
-                m.right.equalTo(-RightMargin)
-                m.left.equalTo(self.attentionBtn.snp.right).offset(Space)
-            }
-            //其他
-            self.view.layoutIfNeeded()
-        }
-        attentionBtn.shrinkAnimation()
-        chatBtn.expandAnimation()
-        print("attentionBtnAction")
-    }
-    
-    @objc func chatBtnAction() {
-        UIView.animate(withDuration: 55 / k) {
-            self.chatBtn.snp.remakeConstraints { m in
-                m.width.height.equalTo(Height)
-                m.centerY.equalToSuperview()
-                m.right.equalTo(-RightMargin)
-            }
-            self.attentionBtn.snp.remakeConstraints { m in
-                m.left.equalToSuperview()
-                m.right.equalTo(self.chatBtn.snp.left).offset(-Space)
-                m.centerY.equalToSuperview()
-                m.height.equalTo(Height)
-            }
-            self.view.layoutIfNeeded()
-        } completion: { complete in
-            
-        }
-        attentionBtn.expandAnimation()
-        chatBtn.shrinkAnimation()
-        print("chatBtnAction")
-    }
 }
 
 extension YDHomeViewController {
